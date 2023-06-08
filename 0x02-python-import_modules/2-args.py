0e24f93a5d33366b80a6693052d91b98d5769a9c
viictoo <provicml@gmail.com>
2023-06-08 14:00:35 +0300

    fix 3 and 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 from sys import argv as av
-ac = len(av) - 1
-if ac == 0:
-    print(f"{ac} arguments.")
-elif ac == 1:
-    print(f"{ac} argument:")
-else:
-    print(f"{ac} arguments:")
-for i in range(ac):
-    print(f'{i + 1}: {av[i+1]}')
+if __name__ == "__main__":
+    ac = len(av) - 1
+    if ac == 0:
+        print(f"{ac} arguments.")
+    elif ac == 1:
+        print(f"{ac} argument:")
+    else:
+        print(f"{ac} arguments:")
+    for i in range(ac):
+        print(f'{i + 1}: {av[i+1]}')
