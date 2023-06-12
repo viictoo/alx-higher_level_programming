c2993bf7e30599da750e1255aa9222bfd9f7d228
viictoo <provicml@gmail.com>
2023-06-12 12:52:31 +0300

    13 palindrome recursive

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,26 +9,30 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *kobe = *head, *hare = *head;
-	listint_t *prev = NULL, *next = NULL;
+	listint_t *current = *head;
 
-	while (hare && hare->next)
-	{
-		hare = hare->next->next;
-		next = kobe->next;
-		kobe->next = prev;
-		prev = kobe;
-		kobe = next;
-	}
-	if (hare)
-		kobe = kobe->next;
+	return (recursive_tree(&current, current));
+}
 
-	while (prev && kobe)
-	{
-		if (prev->n != kobe->n)
-			return (0);
-		prev = prev->next;
-		kobe = kobe->next;
-	}
-	return (1);
+/**
+ * recursive_tree - recursive helper function
+ * @right: pointer to the right node
+ * @left: pointer to the left node
+ * Return: 1 if it is a palindrome, otherwise 0
+ */
+int recursive_tree(listint_t **right, listint_t *left)
+{
+	int current_match, is_sublist_palindrome;
+
+	if (left == NULL)
+		return (1);
+
+	is_sublist_palindrome = recursive_tree(right, left->next);
+	if (!is_sublist_palindrome)
+		return (0);
+
+	current_match = ((*right)->n == left->n);
+	*right = (*right)->next;
+
+	return (current_match);
 }
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -21,5 +21,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
+int recursive_tree(listint_t **right, listint_t *left);
 
 #endif /* LISTS_H */
