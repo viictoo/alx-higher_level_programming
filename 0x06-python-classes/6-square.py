19d1cc44af182e16550ba1061ef46cf599e62988
viictoo <provicml@gmail.com>
2023-06-28 13:31:49 +0300

    fixed edge cases

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -77,11 +77,12 @@ class Square:
 
         if self.size == 0:
             print()
-        for i in range(0, self.__position[1]):
-            print("")
-        for i in range(0, self.__size):
-            for j in range(0, self.__position[0]):
-                print(" ", end="")
-            for k in range(0, self.__size):
-                print("#", end="")
-            print("")
+        else:
+            for i in range(0, self.__position[1]):
+                print("")
+            for i in range(0, self.__size):
+                for j in range(0, self.__position[0]):
+                    print(" ", end="")
+                for k in range(0, self.__size):
+                    print("#", end="")
+                print("")
