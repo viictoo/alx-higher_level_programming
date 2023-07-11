f6b0ff2100370191eef50add8f0dbdb3b07a6f59
viictoo <provicml@gmail.com>
2023-07-12 01:05:02 +0300

    added comments

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -9,17 +9,15 @@ import sys
 
 
 size = 0
+# Initialized an empty dict to store the count of status codes
 status_codes = {}
 valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+# var number of lines processed
 count = 0
 
 
 def print_status_code():
-    """Print accumulated metrics.
-
-    Args:
-        size (int): The accumulated read file size.
-        status_codes (dict): The accumulated count of status codes.
+    """prints statistics since the beginning of Each 10 lines
     """
     print("File size:", size)
     for key in sorted(status_codes):
@@ -28,29 +26,26 @@ def print_status_code():
 
 try:
     for line in sys.stdin:
-        if count == 10:
-            print_status_code()
-            count = 1
-        else:
-            count += 1
+        count += 1
+        if count % 10 == 0:
+                print_status_code()
 
         elements = line.split()
-
         try:
             size += int(elements[-1])
         except (IndexError, ValueError):
             pass
         try:
-            # Get the second-to-last element of `elements` as `status_code`
+            # Get second-to-last element as the <status_code>
             status_code = elements[-2]
             if status_code in valid_codes:
                 if status_code not in status_codes:
-                    # If `status_code` is not in `status_codes`,
-                    # add it as a new key with a count of 1
+                    # If <status_code> is not in status_codes dict,
+                    # add it as a new key, count=1
                     status_codes[status_code] = 1
                 else:
-                    # If `status_code` is already in `status_codes`,
-                    # increment its count by 1
+                    # If <status_code> is already in status_codes dict,
+                    # increment its count
                     status_codes[status_code] += 1
         except IndexError:
             # Ignore IndexError if the element doesn't exist
