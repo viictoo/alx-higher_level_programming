cdb0f36b6ca404a8b4f5fba2c93180e3fdf3e634
Victor Langat <provicml@gmail.com>
2023-08-16 15:12:08 +0300

    pretter format

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -4,6 +4,27 @@
 --     Each record displays: tv_genres.name
 --     Results sorted in ascending order by the genre name
 --     You can use a maximum of two SELECT statement
-SELECT tv_genres.name FROM tv_genres WHERE name NOT IN(SELECT tv_genres.name FROM tv_shows RIGHT OUTER JOIN tv
-_show_genres ON tv_shows.id = tv_show_genres.show_id LEFT OUTER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres
-.id WHERE title = "Dexter") ORDER BY name ASC;
+
+SELECT tv_genres.name
+FROM tv_genres
+WHERE name NOT IN
+(SELECT tv_genres.name
+FROM tv_shows
+RIGHT OUTER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+LEFT OUTER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres
+.id WHERE title = "Dexter")
+ORDER BY name ASC;
+
+    SELECT tv_genres.name
+      FROM tv_genres
+     WHERE tv_genres.name NOT IN (
+    SELECT tv_genres.name
+      FROM tv_genres
+INNER JOIN tv_show_genres
+        ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+	ON tv_show_genres.show_id = tv_shows.id
+     WHERE tv_shows.title = "Dexter")
+  ORDER BY tv_genres.name ASC;
