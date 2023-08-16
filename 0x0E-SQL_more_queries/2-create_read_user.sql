bdbed61101d21cd7e349fdb59a1aaa7625c8e34e
Victor Langat <provicml@gmail.com>
2023-08-16 14:17:14 +0300

    update comments

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,8 @@
+-- a script that creates the MySQL server user user_0d_1.
 
-
+-- user_0d_1 given all privileges on your MySQL server
+-- user_0d_1 password set to user_0d_1_pwd
+-- if the user user_0d_1 already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2';
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
