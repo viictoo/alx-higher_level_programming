3f2b259c8398fe379269962e2ba0d1e21cbe9488
Victor Langat <provicml@gmail.com>
2023-08-16 14:49:18 +0300

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++
@@ -1,6 +0,0 @@
--- script that lists all privileges of the MySQL
--- users user_0d_1 and user_0d_2 on local server
--- DROP USER IF EXISTS 'user_0d_1'@'localhost';
-DROP USER IF EXISTS 'user_0d_2'@'localhost';
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
