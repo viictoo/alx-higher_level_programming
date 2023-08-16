dabc260c7e77a46ecc2d94fbbedc1f173ee1c238
Victor Langat <provicml@gmail.com>
2023-08-16 14:34:53 +0300

    updated flush

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -5,3 +5,4 @@
 --     If the user user_0d_1 already exists, the should not fail
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
