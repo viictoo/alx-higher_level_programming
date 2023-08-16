8ef56098c26d4923935cba7bcf76a730986b5cab
Victor Langat <provicml@gmail.com>
2023-08-16 15:15:49 +0300

    Reformating: Another broken wrap?

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -4,6 +4,12 @@
 --     Results are sorted in descending order by their rating
 --     uses only one SELECT statement
 --     The database name will be passed as an argument of the mysql command
-SELECT tv_genres.name , SUM(rate) AS rating FROM tv_show_ratings, tv_shows, tv_genres, tv_show_genres WHERE tv
-_shows.id = tv_show_ratings.show_id AND tv_shows.id = tv_show_genres.show_id AND tv_genres.id = tv_show_genres.genre_
-id GROUP BY tv_genres.id ORDER BY rating DESC;
+
+SELECT tv_genres.name , SUM(rate)
+AS rating
+FROM tv_show_ratings, tv_shows, tv_genres, tv_show_genres
+WHERE tv_shows.id = tv_show_ratings.show_id
+AND tv_shows.id = tv_show_genres.show_id
+AND tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.id
+ORDER BY rating DESC;
