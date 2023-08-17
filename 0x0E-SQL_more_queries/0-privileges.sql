5b6208b015d1b1841eb476a06af7f3d99b37afdc
Victor Langat <provicml@gmail.com>
2023-08-17 09:10:43 +0300

    checker err

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,9 +1,41 @@
 -- a script that lists all privileges of the MySQL users
--- Revoke unwanted privileges
-REVOKE AUDIT_ABORT_EXEMPT, AUTHENTICATION_POLICY_ADMIN, BACKUP_ADMIN, BINLOG_ADMIN,
-       FIREWALL_EXEMPT, GROUP_REPLICATION_STREAM, PASSWORDLESS_USER_ADMIN,
-       SENSITIVE_VARIABLES_OBSERVER, TELEMETRY_LOG_ADMIN, XA_RECOVER_ADMIN
-FROM 'user_0d_1'@'localhost';
+-- Check if the user exists before revoking privileges
+USE sys;
+DELIMITER //
+CREATE PROCEDURE ModifyUserPrivileges()
+BEGIN
+    DECLARE user_exists INT DEFAULT 0;
 
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+    -- Check if the user exists
+    SELECT COUNT(*) INTO user_exists FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost';
+
+    -- Revoke unwanted privileges if the user exists
+    IF user_exists > 0 THEN
+        REVOKE AUDIT_ABORT_EXEMPT, AUTHENTICATION_POLICY_ADMIN, BACKUP_ADMIN, BINLOG_ADMIN,
+               FIREWALL_EXEMPT, GROUP_REPLICATION_STREAM, PASSWORDLESS_USER_ADMIN,
+               SENSITIVE_VARIABLES_OBSERVER, TELEMETRY_LOG_ADMIN, XA_RECOVER_ADMIN
+        FROM 'user_0d_1'@'localhost';
+    END IF;
+
+    SHOW GRANTS FOR 'user_0d_1'@'localhost';
+    SHOW GRANTS FOR 'user_0d_2'@'localhost';
+    -- -- Grant desired privileges
+    -- GRANT BACKUP_ADMIN, BINLOG_ADMIN
+    -- TO 'user_0d_1'@'localhost';
+
+    -- Create the user and grant privileges if it doesn't exist
+    -- IF user_exists = 0 THEN
+    --     CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+    --     SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
+
+        -- GRANT BACKUP_ADMIN, BINLOG_ADMIN
+END;
+//
+
+DELIMITER ;
+
+-- Call the stored procedure
+CALL ModifyUserPrivileges();
+
+-- SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- SHOW GRANTS FOR 'user_0d_2'@'localhost';
