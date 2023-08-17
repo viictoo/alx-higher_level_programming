66404f48260be106cb7c417310c154252ff76766
Victor Langat <provicml@gmail.com>
2023-08-17 08:56:25 +0300

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,9 @@
 -- a script that lists all privileges of the MySQL users
+-- Revoke unwanted privileges
+REVOKE AUDIT_ABORT_EXEMPT, AUTHENTICATION_POLICY_ADMIN, BACKUP_ADMIN, BINLOG_ADMIN,
+       FIREWALL_EXEMPT, GROUP_REPLICATION_STREAM, PASSWORDLESS_USER_ADMIN,
+       SENSITIVE_VARIABLES_OBSERVER, TELEMETRY_LOG_ADMIN, XA_RECOVER_ADMIN
+FROM 'user_0d_1'@'localhost';
 
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
