eb23fabcdfeaa3f8f7adf2fc742674d4d5f87c18
Victor Langat <provicml@gmail.com>
2023-09-11 14:58:35 +0300

    correct output

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 
 const num = parseInt(process.argv[2]);
-let str = '';
+let str = 'C is fun';
 
 if (num !== 0 && !num) {
   console.log('Missing number of occurrences');
 } else {
-  for (let y = 0; y < num; y++) {
-    str += 'X';
-  }
   for (let i = 0; i < num; i++) {
     console.log(str);
   }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const num = parseInt(process.argv[2]);
+let str = '';
+
+if (num !== 0 && !num) {
+  console.log('Missing size');
+} else {
+  for (let y = 0; y < num; y++) {
+    str += 'X';
+  }
+  for (let i = 0; i < num; i++) {
+    console.log(str);
+  }
+}
