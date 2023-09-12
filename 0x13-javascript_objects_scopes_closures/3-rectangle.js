3c5fe4a81062ac486e71e2241b2aacbc256fa375
Victor Langat <provicml@gmail.com>
2023-09-12 13:30:30 +0300

    fixed newline error

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,9 +1,14 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-  constructor (w, h) { if (w > 0 && h > 0) { this.width = w; this.height = h; } }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
   print () {
-    if (this.width && this.height) { console.log(`${'X'.repeat(this.width)}\n`.repeat(this.height)); }
+    for (let j = 0; j < this.height; j++) { console.log('X'.repeat(this.width)); }
   }
 };
