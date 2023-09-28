b9783ce646e86d4a9c7ac5c8adc7f948f5740077
Victor Langat <provicml@gmail.com>
2023-09-28 19:15:23 +0300

    fix 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # send a get request to an input url
-if [ $# -lt 1 ]; then
-    echo "Usage: $0 <url>"
-    exit 1
-fi
-curl -s -L -X GET "$1" | grep -v '^<'
+curl -s -f  -L -X GET "$1"
