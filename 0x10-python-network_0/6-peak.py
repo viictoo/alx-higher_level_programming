4d2b532ff1912a1340ea0bd107c2b5a923055fc5
Victor Langat <provicml@gmail.com>
2023-09-29 00:08:09 +0300

    test peak pycodestyle fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,7 +4,6 @@
 
 def find_peak(list_of_integers):
     """ get highest number in set """
-    
     if len(list_of_integers) == 0:
         return None
 
@@ -14,7 +13,8 @@ def find_peak(list_of_integers):
 
     mid = size // 2
     peak = list_of_integers[mid]
-    if (peak > list_of_integers[mid - 1]) and (peak > list_of_integers[mid + 1]):
+    if (peak > list_of_integers[mid - 1]) and (
+         peak > list_of_integers[mid + 1]):
         return peak
     elif (peak < list_of_integers[mid - 1]):
         return find_peak(list_of_integers[:mid])
