b0d3920c42798329ba78d07139cc552dbc95e90c
Victor Langat <provicml@gmail.com>
2023-09-29 00:13:47 +0300

    test peak ; elif efficiency

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,7 +9,7 @@ def find_peak(list_of_integers):
 
     if len(list_of_integers) == 1:
         return list_of_integers[0]
-    if len(list_of_integers) == 2:
+    elif len(list_of_integers) == 2:
         return max(list_of_integers)
 
     size = len(list_of_integers)
