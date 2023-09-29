ba5fcf93b7f8696b8b3d61b397cfef993c7308a1
Victor Langat <provicml@gmail.com>
2023-09-29 19:07:14 +0300

    test format

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,11 @@ if __name__ == "__main__":
     with request.urlopen(url) as response:
         response = response.read()
 
+        # print("Body response:")
+        # print(f"\t- type: {type(response)}")
+        # print(f"\t- content: {response}")
+        # print(f"\t- utf8 content: {response.decode('utf-8')}")
         print("Body response:")
-        print(f"\t- type: {type(response)}")
-        print(f"\t- content: {response}")
-        print(f"\t- utf8 content: {response.decode('utf-8')}")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode('utf-8')))
