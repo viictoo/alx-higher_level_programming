1e30202aa956b9c136e78d227988da15d5b0c426
Victor Langat <provicml@gmail.com>
2023-09-29 19:18:43 +0300

    back to intranet settings

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,17 +3,11 @@
 from urllib import request
 
 
-# url = "https://alx-intranet.hbtn.io/status"
+url = "https://alx-intranet.hbtn.io/status"
 if __name__ == "__main__":
-    # with request.urlopen(url) as response:
-    with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+    with request.urlopen(url) as response:
         response = response.read()
-
-        # print("Body response:")
-        # print(f"\t- type: {type(response)}")
-        # print(f"\t- content: {response}")
-        # print(f"\t- utf8 content: {response.decode('utf-8')}")
         print("Body response:")
-        print("\t- type: {}".format(type(response)))
-        print("\t- content: {}".format(response))
-        print("\t- utf8 content: {}".format(response.decode('utf-8')))
+        print(f"\t- type: {type(response)}")
+        print(f"\t- content: {response}")
+        print(f"\t- utf8 content: {response.decode('utf-8')}")
