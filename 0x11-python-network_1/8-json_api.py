fdcbbd1e52470882d4982c19ef279d744febdd74
Victor Langat <provicml@gmail.com>
2023-09-29 17:18:02 +0300

    added more exception

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,19 +8,21 @@ from sys import argv
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
     letter = ""
-
     if len(argv) > 1:
         letter = argv[1]
 
     req = requests.post(url, data={"q": letter})
-    res = req.json()
-    if res:
-        try:
-            if (res.get('id')) is None or (res.get('name')) is None:
+    try:
+        res = req.json()
+        if res:
+            try:
+                if (res.get('id')) is None or (res.get('name')) is None:
+                    print("Not a valid JSON")
+                else:
+                    print(f"[{res.get('id')}] {res.get('name')}")
+            except ValueError:
                 print("Not a valid JSON")
-            else:
-                print(f"[{res.get('id')}] {res.get('name')}")
-        except ValueError:
-            print("Not a valid JSON")
-    else:
-        print("No result")
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
