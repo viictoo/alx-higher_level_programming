4aec831eeaa6cc78360c3f1ba08e1a4ffc9c6b49
Victor Langat <provicml@gmail.com>
2023-09-29 14:47:03 +0300

    codefix: if name main

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,10 +5,11 @@ from urllib import request
 
 url = "https://alx-intranet.hbtn.io/status"
 
-with request.urlopen(url) as response:
-    status = response.read()
+if __name__ == "__main__":
+    with request.urlopen(url) as response:
+        status = response.read()
 
-print("Body response:")
-print("    - type: ", type(status))
-print("    - content: ", status)
-print("    - utf8 content: ", status.decode())
+    print("Body response:")
+    print("    - type: ", type(status))
+    print("    - content: ", status)
+    print("    - utf8 content: ", status.decode())
