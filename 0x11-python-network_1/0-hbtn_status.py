7f1c3d900ba9dd462fb483e8f776d2c0d617aacd
Victor Langat <provicml@gmail.com>
2023-09-29 19:05:33 +0300

    test var name

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,9 +6,9 @@ from urllib import request
 url = "https://alx-intranet.hbtn.io/status"
 if __name__ == "__main__":
     with request.urlopen(url) as response:
-        status = response.read()
+        response = response.read()
 
         print("Body response:")
-        print(f"\t- type: {type(status)}")
-        print(f"\t- content: {status}")
-        print(f"\t- utf8 content: {status.decode('utf-8')}")
+        print(f"\t- type: {type(response)}")
+        print(f"\t- content: {response}")
+        print(f"\t- utf8 content: {response.decode('utf-8')}")
