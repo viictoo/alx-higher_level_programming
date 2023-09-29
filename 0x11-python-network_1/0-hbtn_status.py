eb8bcc1a98dc41b745713014c5ff603b54d9b10c
Victor Langat <provicml@gmail.com>
2023-09-29 18:58:10 +0300

    test commas

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-""" fetch status """
+""""fetch status"""
 from urllib import request
 
 
 url = "https://alx-intranet.hbtn.io/status"
-
 if __name__ == "__main__":
     with request.urlopen(url) as response:
         status = response.read()
